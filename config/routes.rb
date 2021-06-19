394c5cb794363245728fc4017acb82cde98c09f1
horie-toshiya <mmddgpcd1122@gmail.com>
2021-06-19 01:31:04 +0000

    [Add]Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
+devise_for :customers, controllers: {
+  sessions:      'customers/sessions',
+  passwords:     'customers/passwords',
+  registrations: 'customers/registrations'
+}
+
   scope module: :public do
     resources :main_contents, only: [:index, :show]
     resources :events, only: [:index]
